7bf396d15ea37275751f2ed43ad35129a692d3b3
猴子 <a102009102009@gmail.com>
2022-12-11 14:02:34 +0800

    feat id

---
+++ api/service/id.go
@@ -0,0 +1,78 @@
+package service
+
+import (
+	"encoding/base64"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const Epoch int64 = 1669824000000 // 2022-12-01 00:00:00
+const (
+	TimeShift = 12
+	MaxStep   = 1 << TimeShift
+)
+
+// 111111111111111111111111111111111111111111 111111111111
+// 54                                      13 12         0
+//                           timestamp(42bit)  step(12bit)
+
+type (
+	ID       int64
+	SummonID struct {
+		mu    sync.Mutex
+		time  int64
+		step  int64
+		Epoch time.Time
+	}
+)
+
+func NewSummonID() *SummonID {
+	return &SummonID{
+		Epoch: time.Unix(Epoch/1e3, (Epoch%1e3)*1e6),
+		mu:    sync.Mutex{},
+	}
+}
+
+func (s *SummonID) Generate() ID {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	now := time.Since(s.Epoch).Milliseconds()
+	if now == s.time {
+		s.step = (s.step + 1) & MaxStep
+		if s.step == 0 {
+			for now <= s.time {
+				now = time.Since(s.Epoch).Milliseconds()
+			}
+		}
+	} else {
+		s.step = 0
+	}
+
+	return ID((now << TimeShift) | s.step)
+}
+
+func (f ID) Int64() int64 {
+	return int64(f)
+}
+
+func (f ID) String() string {
+	return strconv.FormatInt(int64(f), 10)
+}
+
+func (f ID) Base2() string {
+	return strconv.FormatInt(int64(f), 2)
+}
+
+func (f ID) Bytes() []byte {
+	return []byte(f.String())
+}
+
+func (f ID) Base64() string {
+	return base64.StdEncoding.EncodeToString(f.Bytes())
+}
+
+func (f ID) Time() int64 {
+	return (int64(f) >> TimeShift) + Epoch
+}
