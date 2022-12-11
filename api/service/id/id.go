2663ac63387b78d02dc61cafce6dde88da5de74b
猴子 <a102009102009@gmail.com>
2022-12-11 15:01:23 +0800

    fix

--- api/service/id/id.go
+++ api/service/id/id.go
@@ -3,10 +3,11 @@
 // 54                                      13 12         0
 //                           timestamp(42bit)  step(12bit)
 
-package service
+package id
 
 import (
 	"encoding/base64"
+	"fmt"
 	"strconv"
 	"sync"
 	"time"
@@ -16,6 +17,7 @@ const Epoch int64 = 1669824000000 // 2022-12-01 00:00:00
 const (
 	TimeShift = 12
 	MaxStep   = 1 << TimeShift
+	IDBit     = 54
 )
 
 type SummonID struct {
@@ -39,7 +41,7 @@ func (s *SummonID) Generate() ID {
 
 	now := time.Since(s.Epoch).Milliseconds()
 	if now == s.time {
-		s.step = (s.step + 1) & MaxStep
+		s.step = (s.step + 1) &^ MaxStep
 		if s.step == 0 {
 			for now <= s.time {
 				now = time.Since(s.Epoch).Milliseconds()
@@ -58,7 +60,7 @@ type ID int64
 
 func (f ID) Int64() int64   { return int64(f) }
 func (f ID) String() string { return strconv.FormatInt(int64(f), 10) }
-func (f ID) Base2() string  { return strconv.FormatInt(int64(f), 2) }
+func (f ID) Base2() string  { return fmt.Sprintf("%0*v", IDBit, strconv.FormatInt(int64(f), 2)) }
 func (f ID) Bytes() []byte  { return []byte(f.String()) }
 func (f ID) Base64() string { return base64.StdEncoding.EncodeToString(f.Bytes()) }
 
--- api/service/id/id_test.go
+++ api/service/id/id_test.go
@@ -1,4 +1,4 @@
-package service
+package id
 
 import (
 	"testing"
@@ -10,7 +10,10 @@ func TestSummonID(t *testing.T) {
 	var x, y ID
 	for i := 0; i < 1e4; i++ {
 		y = s.Generate()
-		t.Log(y.Base2())
+		str := y.Base2()
+
+		t.Logf("\033[31m%v\033[0m-\033[34m%v\033[33m%v\033[0m", y.String(), str[:42], str[42:])
+
 		if x == y {
 			t.Errorf("x(%d) & y(%d) are the same", x, y)
 		}
