c56b1a3bb832307a8cae3135609237cc9c594847
猴子 <a102009102009@gmail.com>
2022-12-12 18:11:49 +0800

    feat GetInstanceByNameAndTags

--- api/database/instance.go
+++ api/database/instance.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/a3510377/control-panel/models"
 	"github.com/a3510377/control-panel/service/id"
@@ -29,10 +30,22 @@ func (db *DB) GetInstanceTagsByName(name ...string) []int {
 	return tags
 }
 
-func (db *DB) GetInstanceByTags(tags ...string) []models.Instance {
-	instances, instanceIds := []models.Instance{}, db.GetInstanceTagsByName(tags...)
+func (db *DB) getInstanceByTags(tags ...string) *gorm.DB {
+	return db.Where("id IN ?", db.GetInstanceTagsByName(tags...))
+}
 
-	db.Where("id IN ?", instanceIds).Find(&instances)
+func (db *DB) GetInstanceByTags(tags ...string) []models.Instance {
+	instances := []models.Instance{}
+
+	db.getInstanceByTags(tags...).Find(&instances)
+
+	return instances
+}
+
+func (db *DB) GetInstanceByNameAndTags(name string, tags []string) []models.Instance {
+	instances := []models.Instance{}
+
+	db.getInstanceByTags(tags...).Where("name LIKE ?", fmt.Sprintf("%v%%", name)).Find(&instances)
 
 	return instances
 }
