b8b58eafb6b1773b31c042027b5386bc0de5e410
猴貓 <a102009102009@gmail.com>
2022-12-19 11:45:11 +0800

    fix filename

--- api/routers/private/instance/instances.go
+++ api/routers/private/instance/instances.go
@@ -1,36 +1,13 @@
 package instance
 
 import (
-	"net/http"
-
 	"github.com/a3510377/control-panel/container"
-	"github.com/a3510377/control-panel/database"
 	"github.com/gin-gonic/gin"
 )
 
-const instanceDataKey = "instance_data"
-
-func addInstanceHandlers(container *container.Container, instancePath *gin.RouterGroup) {
-	instancePath.GET("/", func(c *gin.Context) {
-		data := c.MustGet(instanceDataKey).(*database.DBInstance)
-		c.JSON(http.StatusOK, data.Instance)
-	})
-
-	instancePath.PATCH("/", func(c *gin.Context) {
-		var newInstance map[string]any
-
-		data := c.MustGet(instanceDataKey).(*database.DBInstance)
-
-		c.BindJSON(&newInstance)
-		data.Updates(newInstance)
-	})
-
-	instancePath.DELETE("/", func(c *gin.Context) {
-		data := c.MustGet(instanceDataKey).(*database.DBInstance)
-		if err := data.Delete(); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "instance deleted error"})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"error": "instance deleted"})
+func addInstancesHandlers(container *container.Container, app *gin.RouterGroup) {
+	app.POST("/", func(c *gin.Context) {
+		// TODO add create instance
+		c.JSON(200, gin.H{"message": "POST"})
 	})
 }
