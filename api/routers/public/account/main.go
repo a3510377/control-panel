0fc3349482d9fa0c15d4fa06e91b03271cfdf994
猴貓 <a102009102009@gmail.com>
2022-12-24 16:57:59 +0800

    Update main.go

--- api/routers/public/account/main.go
+++ api/routers/public/account/main.go
@@ -1,20 +1,11 @@
 package account
 
 import (
-	"fmt"
-
 	"github.com/a3510377/control-panel/container"
 	"github.com/gin-gonic/gin"
 )
 
-func checkNoToken(c *gin.Context) {
-	fmt.Println("Authorization:", c.GetHeader("Authorization"))
-}
-
 func AddHandler(container *container.Container, app *gin.RouterGroup) {
-	No := app.Group("/", func(ctx *gin.Context) {
-		checkNoToken(ctx)
-	})
-	addCreateHandlers(container, No)
+	addCreateHandlers(container, app)
 	addLoginHandlers(container, app)
 }
