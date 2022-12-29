5f732bb441162db1f8779a630592ee0f359892fe
猴貓 <a102009102009@gmail.com>
2022-12-30 06:36:34 +0800

    feat user info box

--- web/components/Navbar/user.tsx
+++ web/components/Navbar/user.tsx
@@ -1,7 +1,24 @@
+import { Avatar, Box, ListItemText } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 export default function User() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '0 10px 10px',
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar src="" />
+        <Box sx={{ 'padding-left': '8px' }}>
+          <span>{'test'}</span>
+        </Box>
+      </Box>
+
+      <SettingsIcon />
+    </Box>
   );
 }
