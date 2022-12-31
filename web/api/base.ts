853d6d5b0212b36c0a14f3aae7bdc109440715d5
猴貓 <a102009102009@gmail.com>
2022-12-31 18:01:51 +0800

    fix remove transformRequest options

--- web/api/base.ts
+++ web/api/base.ts
@@ -6,18 +6,14 @@ export type ResponseError = { error: string };
 export const RootApi = axios.create({
   baseURL: process.env.API_BASE_URL + '/api',
   headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-  transformRequest: [
-    function (data, headers) {
-      this.params = {
-        l: navigator.language || localStorage.getItem('lang') || 'zh_TW',
-        ...this.params,
-      };
-      headers.set('Authorization', GetToken());
-      return data;
-    },
-    // @ts-ignore
-    ...axios.defaults.transformRequest,
-  ],
+  transformRequest: function (data, headers) {
+    this.params = {
+      l: navigator.language || localStorage.getItem('lang') || 'zh_TW',
+      ...this.params,
+    };
+    headers.set('Authorization', GetToken());
+    return data;
+  },
   transformResponse: (r) => jsonBig().parse(r),
 });
 
