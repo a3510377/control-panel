d12c330d9561ca75116f37a642f0ee2bd7825695
猴貓 <a102009102009@gmail.com>
2022-12-31 17:56:45 +0800

    fix Authorization header is null

--- web/api/base.ts
+++ web/api/base.ts
@@ -7,14 +7,19 @@ export const RootApi =
   (typeof window !== 'undefined' || void 0) &&
   axios.create({
     baseURL: process.env.API_BASE_URL + '/api',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: localStorage.getItem('token') || '',
-    },
-    params: {
-      l: navigator.language || localStorage.getItem('lang') || 'zh-TW',
-    },
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    transformRequest: [
+      function (data, headers) {
+        this.params = {
+          l: navigator.language || localStorage.getItem('lang') || 'zh_TW',
+          ...this.params,
+        };
+        headers.set('Authorization', GetToken());
+        return data;
+      },
+      // @ts-ignore
+      ...axios.defaults.transformRequest,
+    ],
     transformResponse: (r) => jsonBig().parse(r),
   });
 
