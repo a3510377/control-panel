9d6b8427d09725590c303291f012952084a45208
猴貓 <a102009102009@gmail.com>
2022-12-31 08:01:23 +0800

    fix style

--- web/components/home/chart.tsx
+++ web/components/home/chart.tsx
@@ -1,5 +1,5 @@
 import { Overview, OverviewData } from '@/system/overview';
-import { useEffect, useState } from 'react';
+import { useEffect, useId, useState } from 'react';
 import {
   Area,
   AreaChart,
@@ -12,56 +12,22 @@ import {
 } from 'recharts';
 import Title from './title';
 
-export default function Chart() {
-  const [overviewData, setOverviewData] = useState<(OverviewData | void)[]>([]);
-
-  useEffect(() => {
-    Overview()?.then((data) => {
-      if (data && !('error' in data)) setOverviewData(data);
-    });
-
-    const loop = setInterval(async () => {
-      Overview()?.then((data) => {
-        if (data && !('error' in data)) {
-          setOverviewData([...data]);
-        }
-      });
-    }, 10 * 1e3);
-    return () => clearInterval(loop);
-  }, []);
-
-  const getValue = () => {
-    const strTime = (date: Date) => {
-      return [
-        date.getHours().toString().padStart(2, '0'),
-        date.getMinutes().toString().padStart(2, '0'),
-        date.getSeconds().toString().padStart(2, '0'),
-      ].join(':');
-    };
-
-    return overviewData
-      .slice(0, 60)
-      .reverse()
-      .map((d) => {
-        if (d) {
-          return {
-            time: strTime(new Date(d.time)),
-            cpu_usage: d.cpu_usage,
-            mem_usage: d.mem_usage,
-          };
-        }
-      })
-      .fill(void 0, 60);
-  };
-
+export default function Chart(props: {
+  type: string;
+  data: any[];
+  title?: string;
+  color?: string;
+}) {
+  const color = props.color || '#8884d8';
+  const colorID = useId();
   return (
     <>
-      <Title>伺服器資源使用狀態</Title>
+      <Title>{props.title}</Title>
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           width={500}
           height={300}
-          data={getValue()}
+          data={props.data}
           margin={{
             top: 5,
             right: 30,
@@ -70,35 +36,25 @@ export default function Chart() {
           }}
         >
           <defs>
-            <linearGradient id="cpu_usage" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="mem_usage" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            <linearGradient id={colorID} x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={color} stopOpacity={0.8} />
+              <stop offset="95%" stopColor={color} stopOpacity={0} />
             </linearGradient>
           </defs>
           <XAxis dataKey="time" />
-          <Tooltip />
+          <Tooltip
+            formatter={(value) => `${value}%`}
+            labelStyle={{ color: 'black' }}
+          />
           <CartesianGrid strokeDasharray="3 3" />
-          <Legend verticalAlign="top" height={36} />
           <YAxis tickFormatter={(tick) => `${tick}%`} />
           <Area
-            name="內存使用率"
+            name={props.type}
             type="monotone"
-            dataKey="mem_usage"
-            stroke="#82ca9d"
+            dataKey={props.type}
+            stroke={color}
             fillOpacity={1}
-            fill="url(#mem_usage)"
-          />
-          <Area
-            name="CPU 使用率"
-            type="monotone"
-            dataKey="cpu_usage"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#cpu_usage)"
+            fill={`url(#${colorID})`}
           />
         </AreaChart>
       </ResponsiveContainer>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,76 @@
 import Chart from '#/home/chart';
 import Layout from '#/layout';
-import { Container, Grid, Paper } from '@mui/material';
+import { OverviewData, Overview } from '@/system/overview';
+import { Container, Paper } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [overviewData, setOverviewData] = useState<(OverviewData | void)[]>([]);
+
+  useEffect(() => {
+    Overview()?.then((data) => {
+      if (data && !('error' in data)) setOverviewData(data);
+    });
+
+    const loop = setInterval(async () => {
+      // TODO use limit param
+      Overview()?.then((data) => {
+        if (data && !('error' in data)) {
+          setOverviewData([...data]);
+        }
+      });
+    }, 10 * 1e3);
+    return () => clearInterval(loop);
+  }, []);
+
+  const getValue = () => {
+    const strTime = (date: Date) => {
+      return [
+        date.getHours().toString().padStart(2, '0'),
+        date.getMinutes().toString().padStart(2, '0'),
+        date.getSeconds().toString().padStart(2, '0'),
+      ].join(':');
+    };
+
+    return overviewData
+      .slice(0, 60)
+      .reverse()
+      .map((d) => {
+        return (
+          d && {
+            time: strTime(new Date(d.time)),
+            cpu_usage: d.cpu_usage,
+            mem_usage: d.mem_usage,
+          }
+        );
+      })
+      .fill(void 0, 60);
+  };
+
   return (
     <Layout>
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} mb={8} lg={9}>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+                color: 'white',
+              }}
+            >
+              <Chart
+                title="CPU 使用率"
+                type={'cpu_usage'}
+                data={getValue()}
+                color="#56b0f5"
+              />
+            </Paper>
+          </Grid>
+          <Grid xs={6}>
             <Paper
               sx={{
                 p: 2,
@@ -16,7 +79,12 @@ export default function Home() {
                 height: 240,
               }}
             >
-              <Chart />
+              <Chart
+                title="內存使用率"
+                type={'mem_usage'}
+                data={getValue()}
+                color="#8884d8"
+              />
             </Paper>
           </Grid>
         </Grid>
