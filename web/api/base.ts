919786e05e17a93f3e2633c78b0dca56913d2641
猴貓 <a102009102009@gmail.com>
2022-12-31 17:58:41 +0800

    fix remove check window is not void

--- web/api/base.ts
+++ web/api/base.ts
@@ -3,24 +3,22 @@ import jsonBig from 'json-bigint';
 
 export type ResponseError = { error: string };
 
-export const RootApi =
-  (typeof window !== 'undefined' || void 0) &&
-  axios.create({
-    baseURL: process.env.API_BASE_URL + '/api',
-    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-    transformRequest: [
-      function (data, headers) {
-        this.params = {
-          l: navigator.language || localStorage.getItem('lang') || 'zh_TW',
-          ...this.params,
-        };
-        headers.set('Authorization', GetToken());
-        return data;
-      },
-      // @ts-ignore
-      ...axios.defaults.transformRequest,
-    ],
-    transformResponse: (r) => jsonBig().parse(r),
-  });
+export const RootApi = axios.create({
+  baseURL: process.env.API_BASE_URL + '/api',
+  headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+  transformRequest: [
+    function (data, headers) {
+      this.params = {
+        l: navigator.language || localStorage.getItem('lang') || 'zh_TW',
+        ...this.params,
+      };
+      headers.set('Authorization', GetToken());
+      return data;
+    },
+    // @ts-ignore
+    ...axios.defaults.transformRequest,
+  ],
+  transformResponse: (r) => jsonBig().parse(r),
+});
 
 export const GetToken = () => localStorage.getItem('token') || void 0;
