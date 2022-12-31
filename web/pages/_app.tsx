f9ccfe6c00a95beaef9193e6e5be02e0d3e783fb
猴貓 <a102009102009@gmail.com>
2022-12-31 09:19:21 +0800

    feat resize-animation-stopper

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -11,8 +11,19 @@ import { useRouter } from 'next/router';
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   useEffect(() => {
-    const loop = handleDevTools();
+    let resizeTimer: NodeJS.Timeout;
 
+    addEventListener('resize', () => {
+      console.log(document.body);
+
+      document.body?.classList.add('resize-animation-stopper');
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        document.body?.classList.remove('resize-animation-stopper');
+      }, 400);
+    });
+
+    const loop = handleDevTools();
     return () => clearInterval(loop);
   }, []);
   return (
