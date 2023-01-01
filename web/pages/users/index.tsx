9cbd1d95561c90d70001f9e3b424fb49e8d4c6be
猴貓 <a102009102009@gmail.com>
2023-01-01 18:04:53 +0800

    feat

--- web/pages/users/index.tsx
+++ web/pages/users/index.tsx
@@ -1,9 +1,34 @@
 import Layout from '#/layout';
+import {
+  Checkbox,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
 
 export default function UsersPage() {
+  const [users, setUsers] = useState([]);
+
+  const data: string[] = [];
   return (
-    <Layout>
-      <div></div>
+    <Layout rootStyle={{ margin: '1em', width: '100%' }}>
+      <TableHead>
+        <TableRow>
+          <TableCell>
+            <Checkbox></Checkbox>
+          </TableCell>
+          {/* {data.map((d, id) => (
+            <TableCell key={id}>
+              <TableSortLabel></TableSortLabel>
+            </TableCell>
+          ))} */}
+        </TableRow>
+        {/* <Typography></Typography> */}
+      </TableHead>
     </Layout>
   );
 }
